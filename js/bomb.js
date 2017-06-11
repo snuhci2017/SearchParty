e9e9c20029f74631420d436e7f489eeddf8cefdf
Junmo Park <junmohi1@gmail.com>
2017-06-11 18:42:27 +0900

    설문창 ui 완성

--- js/bomb.js
+++ js/bomb.js
@@ -1,5 +1,6 @@
-var bombSeconds = document.querySelector(".bomb-seconds");
+var bombSeconds = $(".bomb-seconds")[0];
 var tl = new TimelineMax()
+.addCallback(resetSlider, 0)
 .from(
   ".card",
   .45,
@@ -33,9 +34,63 @@ var tl = new TimelineMax()
   0.5,
   {rotation: "45deg", y: "+250px", opacity: 0.},
   5
-);
-tl.pause();
+)
+.add(function () { console.log("Time Out"); tl.restart(); }, "+=0")
+;
+// tl.pause();
 
-document.querySelector("#mycontainer").addEventListener("click", function () {
-  tl.restart();
+//$("#mycontainer").click(function () {
+//  tl.restart();
+//});
+
+
+var slider = $('#ex1').slider();
+slider.slider("on", "slideStart", function(){ console.log("Answered"); tl.restart(); });
+
+
+function resetSlider() {
+    setSliderValue(0.5);
+}
+
+resetSlider();
+
+function setSliderValue(ratio) {
+    // soft red : #e34a33
+    // soft blue : #2c7bb6
+    var color;
+    if (ratio < 0.5)
+        color = chroma.mix('#e34a33', 'white', ratio * 2, 'lab');
+    else
+        color = chroma.mix('white', '#2c7bb6', (ratio - 0.5) * 2);
+    //var color = chroma.mix('#e34a33', '#2c7bb6', ratio, 'lab');
+    slider.slider("setValue", parseInt(Math.round(ratio * 100)));
+
+    $('#ex1Slider .slider-selection')
+        .css("background", color);
+    $('#ex1Slider .slider-track')
+        .css("background", color);
+}
+
+var bbox = null;
+var width = null;
+$("#card").mousemove(function (ev) {
+    var elt = $("#ex1Slider")[0];
+    if (!elt) {
+        return;
+    }
+    if (!bbox) {
+        bbox = elt.getBoundingClientRect();
+        width = bbox.right - bbox.left + 1;
+    }
+    var offX = ev.clientX - bbox.left,
+        offY = ev.clientY - bbox.top;
+
+    if (offY <= -10 || offY >= 30)
+        return;
+    var ratio = offX / width;
+    ratio = Math.max(Math.min(ratio, 1),0);
+
+    setSliderValue(ratio);
 });
+
+
